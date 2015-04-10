7682e30130867b5dab6cfdcff3e4237ab08d8c3c
ElendilTheTall <alistoffish@gmail.com>
2015-04-10 16:06:22 +0100

    Testing a second commit

--- app/src/main/java/com/interglobalmegacorp/gittestapplication/MainActivity2Activity.java
+++ app/src/main/java/com/interglobalmegacorp/gittestapplication/MainActivity2Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
     //testing the commit thang
+    //testing it again
 
 public class MainActivity2Activity extends ActionBarActivity {
 
